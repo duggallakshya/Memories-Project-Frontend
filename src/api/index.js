255961a0b81dfc55814b38c3fe8990f8117d6df7
duggallakshya <duggallakshya@gmail.com>
2022-07-22 02:43:23 +0530

    initial commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-
-//https://memories-project-by-lakshya.herokuapp.com/
+const API = axios.create({ baseURL: 'https://memories-project-by-lakshya.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
